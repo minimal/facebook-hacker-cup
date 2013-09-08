68c77a7d6c68a2752de00f1c02ae3ac65b5a9dda
Chris McDevitt <christophermcdevitt@gmail.com>
2013-09-08 23:23:56 +0100

    Start of using core.typed with first task

--- fb-lines/project.clj
+++ fb-lines/project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/core.typed "0.2.4"]]
+  :core.typed {:check [fb-lines.core]}
   :main fb-lines.core)
--- fb-lines/src/fb_lines/core.clj
+++ fb-lines/src/fb_lines/core.clj
@@ -1,20 +1,32 @@
 (ns fb-lines.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.core.typed :refer [ann check-ns typed-deps def-alias ann-datatype
+                                        for> fn> ann-form AnyInteger doseq> cf inst
+                                        letfn> override-method dotimes>]
+             :as t]))
 
+(ann alphabet (t/Set Character))
 (def alphabet (set "abcdefghijklmnopqrstuvwxyz"))
 
+(ann clojure.core/sort-by [[Any -> Boolean] t/Seqable -> (t/Seqable String)])
+(ann calc-line [String -> Number])
 (defn calc-line [line]
-  (let [ranks (reverse 
-               (sort-by second 
-                        (frequencies 
+  (let [ranks (reverse
+               (sort-by second
+                        (frequencies
                          (filter alphabet
-                                 (clojure.string/lower-case line)))))]
-    (apply + (for [[[_ mult] score] (zipmap ranks (range 26 1 -1))]
+                                 (clojure.string/lower-case line)))))
+        zipranks (zipmap ranks (range 26 1 -1))
+        _ (ann-form zipranks (t/Map t/Seqable  AnyInteger))]
+    (apply + (for> :- Number
+                   [[[_ mult] score] :-  (t/Map String AnyInteger) zipranks]
                (* mult score)))))
 
+(ann lines-from-file [String -> (t/Seqable String)])
 (defn lines-from-file [filename]
   (clojure.string/split-lines (slurp filename)))
 
+(ann -main ['[String] -> Any])
 (defn -main
   [& args]
   (let [scores (map calc-line (rest (lines-from-file (first args))))]
