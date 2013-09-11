8a72ad33ed176d39943f3a2923b7832e5dff326c
Chris McDevitt <christophermcdevitt@gmail.com>
2013-09-11 18:30:07 +0100

    initial core.typed on smileys works with some no-checks

--- fb-smileys/src/fb_smileys/core.clj
+++ fb-smileys/src/fb_smileys/core.clj
@@ -1,24 +1,35 @@
 (ns fb-smileys.core
   (:use [clojure.test])
+  (:require [clojure.core.typed :refer [ann check-ns typed-deps def-alias ann-datatype
+                                        for> fn> ann-form AnyInteger doseq> cf inst
+                                        letfn> override-method dotimes> loop>]
+             :as t])
   (:gen-class))
 
+(ann reg java.util.regex.Pattern)
 (def reg #"(\(.*?)(:\))?(:\()?(\))")
 
+(ann clojure.string/replace [String String String -> String])
+(ann rm-smileys [String -> String])
 (defn rm-smileys [line]
   (-> line
     (clojure.string/replace ":)" "")
     (clojure.string/replace ":(" "")))
 
+(ann clojure.core/neg? [AnyInteger -> Boolean])
+(ann even-parens? [String -> Boolean])
 (defn even-parens? [line]
-  (loop [score 0 chars (seq line)]
+  (loop> [score :- AnyInteger 0
+          chars :- (U (t/Seq java.lang.Character) nil) (seq line)]
     (case (first chars)
       \( (recur (inc score) (rest chars))
-      \) (if (neg? (dec score))
+      \) (if (#'neg? (dec score))
            false
            (recur (dec score) (rest chars)))
       nil (zero? score)
       (recur score (rest chars)))))
 
+(ann  balanced? [String -> Boolean])
 (defn balanced? [line]
   (if-let [matches (re-find reg line)]
     (let [m1 (first matches)
@@ -27,6 +38,7 @@
       (balanced? newline))
     (even-parens? (rm-smileys line))))
 
+(ann ^:no-check tests [ -> Boolean])
 (defn tests []
   (is (= false (balanced? ")(")))
   (is (= true (balanced? "hacker cup: started :):)" )))
@@ -34,13 +46,16 @@
   (is (= false (balanced? ":((")))
   (is (= true (balanced? "(:)"))))
 
+(ann yesno-map (HMap Boolean String))
 (def yesno-map
   {true "YES" false "NO"})
 
+(ann ^:no-check parse-file [String -> Any])
 (defn parse-file [filename]
   (doseq [[idx line] (map-indexed vector (rest (clojure.string/split-lines (slurp filename))))]
     (println (str "Case #" (inc idx) ": " (yesno-map (balanced? line))))))
 
+(ann -main [String * -> Any])
 (defn -main
   [& args]
   (if-let [filename (first args)]
